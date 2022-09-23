2946e05c47bb5375d4f6631263677ceb8f75fb07
Greg Fulbright <gregory2fulbright@gmail.com>
2022-09-23 17:04:09 -0600

    json

--- demos/async/js/app.js
+++ demos/async/js/app.js
@@ -5,13 +5,49 @@ function toggleBackground() {
 }
 
 function callAlert(m) {
-    alert(m)
-}
+    if (m) {
+        alert(m)
+    }
+    else {
+        alert("Thanks for calling")
 
-// setTimeout(toggleBackground, 2000)
+    }
+}
 
 for (let i=0; i<10; i++) {
-    setTimeout(toggleBackground, 500 * i)
+    setTimeout(toggleBackground, 100 * i)
 }
 
-setTimeout(() => {callAlert("Watch it, buddy.")}, 3000)
+
+// setTimeout(callAlert, 1000)
+// setTimeout(() => {callAlert("Watch it, buddy.")}, 3000)
+
+const myspan = document.createElement("span")
+// body.append(myspan)
+body.insertBefore(myspan, document.querySelector("script"))
+myspan.innerText = 0
+myspan.style.fontSize="5em"
+
+let count = 0
+
+const countup = () => {
+    myspan.innerText = count++
+}
+
+setInterval(countup, 500)
+
+
+// AJAX: Axync Javascript And SML (or JSON)
+function getUser() {
+    let xhr = new XMLHttpRequest
+    xhr.open("GET", "https://randomuser.me/api")
+    xhr.send()
+    xhr.addEventListener("load", ()=>{
+        let userData = JSON.parse(xhr.response)
+        apiData = document.getElementById("apiData")
+        apiData.innerHTML = `<p> ${userData.results[0].gender} </p>`
+    })
+}
+
+myBtn = document.getElementById("myBtn")
+myBtn.addEventListener("click", getUser)
