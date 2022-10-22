d929e45a086da24741c9bdebe3f8d47c4df60767
Greg Fulbright <gregory2fulbright@gmail.com>
2022-10-22 17:03:29 -0600

    test 11

--- LAMP-LAB/LAMP-LAB/php/add.php
+++ LAMP-LAB/LAMP-LAB/php/add.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Listing</title>
+<title>User Listing</title>
 </head>
 <body>
     <h1>Added</h1>
@@ -11,7 +11,7 @@
 <!-- 
     NOTE: this is our backend (server side) code. 
     1. User cannot see this code -- unlike HTML/CSS/JavaScript
-    2. This is how we will do database opperations (DB is also on server)
+    2. This is how we will do database operations (DB is also on server)
 -->    
 
 <?php
@@ -44,6 +44,13 @@ if ($conn->query($sql) === TRUE) {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
+// $result = $conn->query("SELECT firstname FROM randuser")
+// // output data of each row
+// while($row = $result->fetch_assoc()) {
+//   echo "First Name: " . $row["firstname"]. "<br>";
+// }
+
+
 $conn->close();
 
 ?>
@@ -52,4 +59,10 @@ $conn->close();
     <button onclick="history.back()">Back</button>
 
 </body>
+
+<footer>
+  <p>
+    Bootstrap was used here in some capacity. G Fulbright. 20 October 2022.
+  </p>
+</footer>
 </html>
